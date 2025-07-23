30a5a54215645236d15ace49877e20b42ee9783c
Bruna Mota Barbosa <66503956+brunamota@users.noreply.github.com>
2025-07-22 21:14:37 -0300

    Update IntegraçãoComNumPy.py

--- M6/MatPlotLib/IntegraçãoComNumPy.py
+++ M6/MatPlotLib/IntegraçãoComNumPy.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x = np.linspace(0, 10, 100)  # 100 valores entre 0 e 10
 y = np.sin(x)
 
-plt.plot(x, y, label="asyncio.exceptions.SendfileNotAvailableError(x)")
+plt.plot(x, y, label="Sen(x)")
 plt.title("Função Senoidal")
 plt.xlabel("x")
 plt.ylabel("sen(x)")
